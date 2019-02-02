e609dac3737261189f04be0f4e1d44d243ea672d
Thomas Lacroix <t.f.m.lacroix@gmail.com>
2019-02-02 14:36:43 +0100

    :tada: added bundler

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './css/style.styl'
+
+import './css/reset.styl'
+
+import * as THREE from 'three'
+
+/**
+ * Loop
+ */
+const loop = () =>
+{
+    window.requestAnimationFrame(loop)
+
+    // Renderer
+    
+}
+loop()
+
