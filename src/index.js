bd5c57d98e17d232521cb71732a97596df3c3943
Thomas Lacroix <t.f.m.lacroix@gmail.com>
2019-05-20 13:51:37 +0200

    :sparkles: added lots

--- src/index.js
+++ src/index.js
@@ -204,7 +204,37 @@ window.addEventListener('mousemove', (_event) =>
     cursor.style.top = `${cursor_m.y * 100}%`
 })
 
-
+/**
+ * Ease in 
+ */
+let EasingFunctions = {
+    // no easing, no acceleration
+    linear: function (t) { return t },
+    // accelerating from zero velocity
+    easeInQuad: function (t) { return t*t },
+    // decelerating to zero velocity
+    easeOutQuad: function (t) { return t*(2-t) },
+    // acceleration until halfway, then deceleration
+    easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+    // accelerating from zero velocity 
+    easeInCubic: function (t) { return t*t*t },
+    // decelerating to zero velocity 
+    easeOutCubic: function (t) { return (--t)*t*t+1 },
+    // acceleration until halfway, then deceleration 
+    easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+    // accelerating from zero velocity 
+    easeInQuart: function (t) { return t*t*t*t },
+    // decelerating to zero velocity 
+    easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+    // acceleration until halfway, then deceleration
+    easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+    // accelerating from zero velocity
+    easeInQuint: function (t) { return t*t*t*t*t },
+    // decelerating to zero velocity
+    easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+    // acceleration until halfway, then deceleration 
+    easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+  }
 /**
  * Post processing
  */
@@ -329,6 +359,11 @@ const loop = () =>
        speed = 5
        increment = 0
     }
+    //ease-in 
+    camera.position.x = EasingFunctions.easeOutQuad(cursor_m.x) * 400
+    camera.position.y = EasingFunctions.easeOutQuad(cursor_m.y) * 400
+    
+
     // Scroll bar height 
     ratio = project_container.offsetHeight/sizes.height
     scroll__bar.style.height = `${100/(ratio)}%`
